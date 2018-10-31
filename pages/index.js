a87627a0811a663cd2d2e9ba350ad92c5f08ab58
Vanessa Alves <vanessa.182@hotmail.com>
2018-10-31 12:54:25 -0300

    texto

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,34 @@ const Home = () =>
            
             <button className="div__button">Fale com nossos consultores</button>
         </div>
+        <div>
+            <h3>Como geramos <span>impacto no seu negócio</span></h3>
+            <ul>
+                <li>
+                     <h3>Análise <span>empresarial</span></h3>
+                     <p>O ritimo de crescimento do negócio estagnou? Você já parou
+                         para pensar que o problema pode estar na gestão corporativa? Por mais
+                         delorido que seja para um CEO ou diretor ouvir isso, as falhas no processo
+                         de gestão das companhias são grandes responsáveis pelo não atingimento
+                         de metas,DOI e demais problemas organizacionais que possam existir. Nós
+                         te apoiamos a virar essa página e retomar o crescimento.
+                         <b>Saiba mais.</b>
+                     </p>
+                </li>
+                <li>
+                <h3><span>Software de gestão</span>Custumizado</h3>
+                     <p>Para fechar o tripé necessário para transformar a sua
+                         empresa e acelerar os resultados, é importante
+                         investir em tecnologia. Esqueça o controle manual
+                         e veja como a Solid pode lhe ajudar nessa etapa.
+                         <b>Saiba mais.</b>
+                     </p>
+                </li>
+                <li>
+                    <h3>Como geramos <span>impacto no seu negócio</span></h3>
+                </li>
+            </ul>
+        </div>
     </section>
 
 export default connect(state => state)(Home);
