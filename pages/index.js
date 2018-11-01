94f83222ee7bed567a8a8531dd1596465b912a47
Vanessa Alves <vanessa.182@hotmail.com>
2018-11-01 18:28:15 -0300

    solid

--- pages/index.js
+++ pages/index.js
@@ -58,15 +58,69 @@ const Home = () =>
                 <li className="blog__artigo artigo0">
                     <img  className="blog__foto" src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>    
                 </li>
-                <li className="blog__foto">
-                    <img  src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>
+                <li className="blog__artigo artigo1">
+                <figure>
+                <img  className="blog__foto" src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>    
+                </figure>
+                    <div></div>
                 </li>
-                <li className="blog__foto">
-                    <img  src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>
+                <li className="blog__artigo artigo2">
+                    <img className="blog__foto1" src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>
+                    <div className="blog__text">
+                    <p className="blog__title"><b>O titulo do artigo do blog deve conter até 3 linhas</b></p>
+                    <p className="blog__date"><img className="blog__foto2" src="/static/img/calendar.svg"/> 23 de setembro 2018</p>
+                    <p className="blog__post">Para fechar o tripé necessário para transformar a sua empresa e acelerar os resultados, é importante investir em tecnologia. Esqueça o controle manual e veja como a Solid pode lhe ajudar nessa etapa.</p>
+                    </div>
                 </li>
             </ul>
-            <button className="header__button cases">Conheça nossos blogs</button>
+            <button className="header__button buttonblog">Conheça nossos blogs</button>
         </section> 
+        <section className="conheça">
+        <div className="conheça__info">
+            <h3 className="conheça__h3">Conheça mais conteúdos para transformar o seu negócio</h3>
+            <button className="conheça__button">Conheça nossos blogs</button>
+        </div>
+        <figure className="conheça__figure">
+            <img className="conheça__img" src="/static/img/book.png"/> 
+        </figure>
         </section>
-
+        <section className="equipe">
+        <h3 className="impacto__title">Nossa<b> Equipe</b></h3>
+            <ul className="equipe__list">
+                <li className="equipe__item">
+                    <figure className="equipe__perfil">
+                        <img  className="equipe__img" src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>   
+                    </figure>
+                    <div className="equipe__info">
+                        <b>Patrícia Bogalheira</b>
+                        <p className="equipe__cargo">Diretora de Treinamento</p>
+                        <p className="equipe__post">Para fechar o tripé necessário para transformar a sua empresa e acelerar os resultados, é importante investir em tecnologia. Esqueça o controle manual e veja como a Solid pode lhe ajudar nessa etapa.</p>
+                        </div>
+                </li>
+                <li className="equipe__item">
+                    <figure className="equipe__perfil">
+                         <img  className="equipe__img" src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>   
+                    </figure>
+                    <div className="equipe__info">
+                        <b>Patrícia Bogalheira</b>
+                        <p className="equipe__cargo">Diretora de Treinamento</p>
+                         <p className="equipe__post">Para fechar o tripé necessário para transformar a sua empresa e acelerar os resultados, é importante investir em tecnologia. Esqueça o controle manual e veja como a Solid pode lhe ajudar nessa etapa.</p>
+                    </div>
+                </li>
+                <li className="equipe__item">
+                    <figure className="equipe__perfil">
+                         <img  className="equipe__img" src="/static/img/wallpapersden.com_chris-pine-handsome-pics_1024x1024.jpg"/>   
+                    </figure>
+                    <div className="equipe__info">
+                        <b>Patrícia Bogalheira</b>
+                        <p className="equipe__cargo">Diretora de Treinamento</p>  
+                        <p className="equipe__post">Para fechar o tripé necessário para transformar a sua empresa e acelerar os resultados, é importante investir em tecnologia. Esqueça o controle manual e veja como a Solid pode lhe ajudar nessa etapa.</p>
+                    </div>
+                </li>
+            </ul>
+            <button className="header__button  buttonblog">Saiba mais</button>
+        </section>
+</section>                
+    
 export default connect(state => state)(Home);
+
