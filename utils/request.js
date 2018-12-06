9531310e9ac0161cc9b1c13218c3b93a1c291862
Jefferson <jefferson.barbosa105@gmail.com>
2018-12-06 03:43:31 -0200

    url api

--- utils/request.js
+++ utils/request.js
@@ -1,4 +1,4 @@
-const prefix = "http://ec2-18-207-92-198.compute-1.amazonaws.com";
+const prefix = "http://ec2-18-207-92-198.compute-1.amazonaws.com:1337";
 
 export const request = (url) => 
     new Promise(async (resolve, reject) => {
