648ae188679989cf16d15c03855fda570b191eb0
Jefferson Barbosa <jefferson.barbosa105@gmail.com>
2021-10-26 10:44:11 -0300

    new domain

--- utils/request.js
+++ utils/request.js
@@ -1,19 +1,19 @@
 import fetch from 'isomorphic-fetch';
-const prefix = "https://adminsolid.co.vu";
+const prefix = 'https://admin.solidsolucoes.com.br';
 
-export const request = (url) => 
+export const request = (url) =>
     new Promise(async (resolve, reject) => {
         try {
-            if(typeof window == 'undefined') {
+            if (typeof window == 'undefined') {
                 let res = await fetch(`${prefix}/${url}`);
                 let data = await res.json();
                 resolve(data);
-            }else {
+            } else {
                 let res = await fetch(`/request/${url}`);
                 let data = await res.json();
                 resolve(data.data);
             }
-        }catch(err) {
+        } catch (err) {
             reject(err);
         }
     });
